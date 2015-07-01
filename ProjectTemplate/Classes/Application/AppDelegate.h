dd67bde1a55dd3d7ce343446c6724179c22d4438
Olexandr Poburynnyi <olexandr.poburynnyi@lemberg.co.uk>
2015-07-01 10:19:26 +0300

    Add iOS project template folder structure

---
+++ ProjectTemplate/Classes/Application/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lemberg
+//
+//  Created by Macbook on 5/14/15.
+//  Copyright (c) 2015 Macbook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
